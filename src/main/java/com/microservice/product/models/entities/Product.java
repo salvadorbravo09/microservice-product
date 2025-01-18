6b87a78b64e9cb7f9ed9f8287c1320b63d7d9edd
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-01-17 23:38:18 -0300

    feat: create product entity

---
+++ src/main/java/com/microservice/product/models/entities/Product.java
@@ -0,0 +1,29 @@
+package com.microservice.product.models.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(columnDefinition = "BIT(1) default 0")
+    private Boolean status;
+}
