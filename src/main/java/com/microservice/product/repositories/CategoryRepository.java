adae6b43a00e769a367f33c3c8d08e95a711ae9f
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-01-18 22:47:41 -0300

    feat: add category repository from JpaRepository

---
+++ src/main/java/com/microservice/product/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.microservice.product.repositories;
+
+import com.microservice.product.models.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
