88bf1a67ce50c9cbba911498ea758e56298ef06b
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-01-21 23:48:55 -0300

    feat: add validation annotation in CreateProductRequest dto

--- src/main/java/com/microservice/product/models/dtos/CreateProductRequest.java
+++ src/main/java/com/microservice/product/models/dtos/CreateProductRequest.java
@@ -1,11 +1,24 @@
 package com.microservice.product.models.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Builder
 public class CreateProductRequest {
 
+    @NotEmpty(message = "The field name cannot be empty or null")
     private String name;
+
     private String description;
+
+    @NotNull(message = "The field price cannot be null")
     private BigDecimal price;
+
+    @NotNull(message = "The field category_id cannot be null")
     private Long categoryId;
 }
