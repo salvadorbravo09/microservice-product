fffc46f05238a387fb9484cd450e8cffc5f2ea73
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-01-23 22:50:42 -0300

    feat: add Constants class with status constants

---
+++ src/main/java/com/microservice/product/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.microservice.product.utils;
+
+public class Constants {
+
+    public static final String ACTIVE_STATUS = "ACTIVE";
+    public static final String INACTIVE_STATUS = "INACTIVE";
+}
