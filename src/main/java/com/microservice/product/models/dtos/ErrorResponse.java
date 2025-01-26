f957ecea7f72410b9c526adc7abee2ac671bf8c7
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-01-26 17:45:25 -0300

    feat: create dto for ErrorResponse

---
+++ src/main/java/com/microservice/product/models/dtos/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.microservice.product.models.dtos;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class ErrorResponse {
+
+    private String code;
+    private HttpStatus status;
+    private String message;
+    private List<String> detailMessages;
+    private LocalDateTime timeStamp;
+}
